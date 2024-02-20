bf00a24e87277cbbf309b9f7559cfefbfbb1e5a5
delvin988 <yustin.delvin@gmail.com>
2024-02-20 21:18:25 +0700

    feat: add login and register feature

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,8 +1,41 @@
+const { comparePassword } = require("../helpers/bcrypt");
+const { signToken } = require("../helpers/jwt");
+const {User, Item, Bid, sequelize} = require("../models/index")
+
 module.exports = class AuthController {
   static async register(req, res, next) {
     try {
+      const user = await User.create(req.body);
+      const data = await User.findOne({
+        where: { id: user.id },
+        attributes: {
+          exclude: ["password", "updatedAt", "createdAt"],
+        },
+      });
+
+      res.status(201).json(data);
     } catch (error) {
       next(error);
     }
   }
+
+  static async login(req, res, next) {
+    try {
+       const { email, password } = req.body;
+       if (!email) throw { name: "EmailRequired" };
+       if (!password) throw { name: "PasswordRequired" };
+
+       const user = await User.findOne({ where: { email } });
+       if (!user) throw { name: "InvalidUser" };
+
+       const comparedUser = comparePassword(password, user.password);
+       if (!comparedUser) throw { name: "InvalidUser" };
+
+       const access_token = signToken({ id: user.id });
+
+       res.status(200).json({ message: "Success Login", access_token });
+    } catch (error) {
+       next(error);
+    }
+ }
 };
