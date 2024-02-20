88fea0362bfa42431d31cb7c5bf5e60ab6fe7644
zaracama <cucigudang576@gmail.com>
2024-02-20 20:25:34 +0700

    chore: Setup-Model-Required

--- server/models/item.js
+++ server/models/item.js
@@ -1,22 +1,63 @@
 "use strict";
 const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Item extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Item.hasMany(models.Bid);
     }
   }
+
   Item.init(
     {
-      name: DataTypes.STRING,
-      description: DataTypes.STRING,
-      imageUrl: DataTypes.STRING,
-      price: DataTypes.INTEGER,
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: `Name is required`
+          },
+          notEmpty: {
+            msg: `Name is required`
+          },
+        },
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: `Description is required`
+          },
+          notEmpty: {
+            msg: `Description is required`
+          },
+        },
+      },
+      imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: `Image URL is required`
+          },
+          notEmpty: {
+            msg: `Image URL is required`
+          },
+        },
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: `Price is required`
+          },
+          notEmpty: {
+            msg: `Price is required`
+          },
+        },
+      },
     },
     {
       sequelize,
