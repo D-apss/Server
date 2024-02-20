4f5686916d272ea15ffe313c5e9760c8308634f4
Rifandye <rilfransyah@gmail.com>
2024-02-21 01:24:24 +0700

    fix: router

--- server/routes/AdminFeature.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const AdminController = require("../controllers/AdminController");
-const router = express.Router();
-
-
-
-router.post("/item", AdminController.createItem);
-
-module.exports = router;
