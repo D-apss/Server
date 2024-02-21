5e85b4e85d5d2ee949689f9d4d1d97584c196bbf
delvin988 <yustin.delvin@gmail.com>
2024-02-21 20:41:40 +0700

    feat: setup socket io on app.js

--- server/app.js
+++ server/app.js
@@ -3,10 +3,17 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
+const{createServer} = require("http") //socket io setup
+const{Server} = require ("socket.io") //socket io setup
+
 const app = express();
+const httpServer = createServer(app) //socket io setup
+const io = new Server(httpServer) //socket io setup
+
 const port = 3000;
 const router = require("./routes/router");
 const errorHandler = require("./middlewares/ErrorHandler");
+
 
 const cors = require("cors");
 app.use(cors());
@@ -17,8 +24,13 @@ app.use(express.json());
 app.use(router);
 app.use(errorHandler);
 
-app.listen(port, () => {
+
+io.on("connection", (socket) => {
+
+})//socket io setup
+
+httpServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
+}); //socket io setup ganti app dengan httpServer
 
 module.exports = app;
